71a529bb9ba11f1e47cbc71b077cde6ee1c058ed
Tommi Vainikainen <thv@iki.fi>
2018-03-23 19:56:05 +0200

    Refactor tests to allow adding more sub-unit test files

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,34 @@
 
 const Knex = require('knex');
 
+function* personGenerator(start, end) {
+  let i = start;
+
+  const firstnames = ['Jennifer', 'Sylvester', 'Tommy', 'John'];
+
+  while(i <= end) {
+    yield {id: i,
+           firstname: firstnames[i % firstnames.length],
+           lastname: `Stallone`
+          };
+    i++;
+  }
+}
+
+function* numberGenerator(start, end, incr = 7) {
+  let i = start;
+
+  while(i <= end) {
+    yield {id: i,
+           thousands: Math.floor(i / 1000) % 10,
+           hundreds: Math.floor(i / 100) % 10,
+           tens: Math.floor(i / 10) % 10,
+           ones: i % 10
+          };
+    i += incr;
+  }
+}
+
 describe('database tests', () => {
   const dbConfigs = [
     {
@@ -26,7 +54,48 @@ describe('database tests', () => {
     let knex = Knex(knexConfig);
 
     describe(knexConfig.client, () => {
-       return require('./paging')(knex);
+      before(() => {
+        return knex.schema.dropTableIfExists('Person');
+      });
+
+      before(() => {
+        return knex.schema.createTable('Person', table => {
+          table.increments('id').primary();
+          table.string('firstname');
+          table.string('lastname');
+        });
+      });
+
+      before(() => {
+        return knex('Person')
+          .then(function () {
+            // Inserts seed entries
+            return knex('Person').insert([...personGenerator(1, 29)]);
+          });
+      });
+
+      before(() => {
+        return knex.schema.dropTableIfExists('Number');
+      });
+
+      before(() => {
+        return knex.schema.createTable('Number', table => {
+          table.increments('id').primary();
+          table.integer('thousands');
+          table.integer('hundreds');
+          table.integer('tens');
+          table.integer('ones');
+        });
+      });
+
+      before(() => {
+        return knex('Number')
+          .then(function () {
+            return knex('Number').insert([...numberGenerator(1, 1111)]);
+          });
+      });
+
+      require('./paging')(knex);
     });
 
     return knex;
--- tests/paging.js
+++ tests/paging.js
@@ -2,77 +2,8 @@ const expect = require('expect.js');
 const Model = require('objection').Model;
 const keysetPaginationRequire = require('..');
 
-function* personGenerator(start, end) {
-  let i = start;
-
-  const firstnames = ['Jennifer', 'Sylvester', 'Tommy', 'John'];
-
-  while(i <= end) {
-    yield {id: i,
-           firstname: firstnames[i % firstnames.length],
-           lastname: `Stallone`
-          };
-    i++;
-  }
-}
-
-function* numberGenerator(start, end, incr = 7) {
-  let i = start;
-
-  while(i <= end) {
-    yield {id: i,
-           thousands: Math.floor(i / 1000) % 10,
-           hundreds: Math.floor(i / 100) % 10,
-           tens: Math.floor(i / 10) % 10,
-           ones: i % 10
-          };
-    i += incr;
-  }
-}
-
 module.exports = knex => {
   describe('plugin tests', () => {
-    before(() => {
-      return knex.schema.dropTableIfExists('Person');
-    });
-
-    before(() => {
-      return knex.schema.createTable('Person', table => {
-        table.increments('id').primary();
-        table.string('firstname');
-        table.string('lastname');
-      });
-    });
-
-    before(() => {
-      return knex('Person')
-        .then(function () {
-          // Inserts seed entries
-          return knex('Person').insert([...personGenerator(1, 29)]);
-        });
-    });
-
-    before(() => {
-      return knex.schema.dropTableIfExists('Number');
-    });
-
-    before(() => {
-      return knex.schema.createTable('Number', table => {
-        table.increments('id').primary();
-        table.integer('thousands');
-        table.integer('hundreds');
-        table.integer('tens');
-        table.integer('ones');
-      });
-    });
-
-    before(() => {
-      return knex('Number')
-        .then(function () {
-          return knex('Number').insert([...numberGenerator(1, 1111)]);
-        });
-    });
-
     it('should return .limit() rows per call with defaults', () => {
       const keysetPagination = keysetPaginationRequire();
       class Person extends keysetPagination(Model) {

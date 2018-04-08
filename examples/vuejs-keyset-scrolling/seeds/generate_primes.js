f40fa2ca51d21b56ea3ac8f3dc8f80958b6e4e1d
Tommi Vainikainen <thv@iki.fi>
2018-04-08 13:16:57 +0300

    Faster prime generation

--- examples/vuejs-keyset-scrolling/seeds/generate_primes.js
+++ examples/vuejs-keyset-scrolling/seeds/generate_primes.js
@@ -1,6 +1,6 @@
 function* primeGenerator() {
   function isPrime(num) {
-    for (var i = 2; i < num; i++) {
+    for (var i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) {
         return false;
       }
